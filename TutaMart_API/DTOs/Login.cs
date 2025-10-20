fbfdfc640f45dfd8fe19ed276f5f051fb959cd14
tutai13 <taidtpd10687@gmail.com>
2025-10-20 09:41:01 +0700

    begin

---
+++ TutaMart_API/DTOs/Login.cs
@@ -0,0 +1,6 @@
+﻿namespace TutaMart_API.DTOs
+{
+    public class Login
+    {
+    }
+}
---
+++ TutaMart_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TutaMart_API.Models;
+
+namespace TutaMart_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+}
